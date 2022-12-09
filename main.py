e30f7b47768dd5f934d2b4f44a05d4cdb9b2babe
DITTOKOPF (Jan) <116339671+DITTOKOPF@users.noreply.github.com>
2022-12-09 08:19:17 +0000

    everything

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Test")
+#Lügendetektor...
+
+import statistics
+
+list = [12, 24, 36, 48, 60]
+print("List : " + str(list))
+
+st_dev = statistics.pstdev(list)
+print("Standard deviation of the given list: " + str(st_dev))
+
+def std():
+  
